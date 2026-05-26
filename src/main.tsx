6b1a10cf85cd63a0f617b2ab741cc913aa2267e2
roy-gunjan743 <roygunjan259@gmail.com>
2026-05-27 01:54:05 +0530

    fix: import styles.css in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClient } from "@tanstack/react-query";
 
 import { routeTree } from "./routeTree.gen";
+import "./styles.css";
 
 const queryClient = new QueryClient();
 
